867e8d89205638edd34735193687ddd4f17e7573
Joel Graycar <jgraycar@berkeley.edu>
2014-07-18 10:30:17 -0700

    did work on data-cleaning; almost done

--- data-cleaning.r
+++ data-cleaning.r
@@ -3,22 +3,28 @@ ling.data = read.table('lingData.txt', head=TRUE)
 # omitted every question have been removed. Store the **number** of
 # observations that you omitted as the variable <n.no.response>
 
-# n.no.response <- your code here
+sums = rowSums(ling.data[, 5:71])
+no.responses = (sums == 0)
+n.no.response <- sum(no.responses)
+nonempty.responses = ling.data[!no.responses, ]
 
 # plot a histogram of the number of omitted responses for each observation
 # after removing observations that omitted all questions
 
-
+omitted = (nonempty.responses[, 5:71] == 0)
+num.omitted = rowSums(omitted)
+hist(num.omitted)
 
 # using your subset (with observations that responded to no questions
 # removed) find the 99th percentile cutoff for number of questions
 # omitted. Remove all observations with more than this number of omitted
 # questions.
 
+cutoff = quantile(num.omitted, .99)
+remove.obs = (num.omitted > cutoff)
+valid.responses = nonempty.responses[!remove.obs, ]
+
 # save the subset of remaining observations in a file named
 # "ling-data-clean.data" 
 
-## My Notes:
-## Get number of possible responses by calling max on the column
-## Get number of non-responded by calling sum(apply(data, 1, '>', 0)
-## If == 0, can omit
+write.table(valid.responses, file="ling-data-clean.data")
