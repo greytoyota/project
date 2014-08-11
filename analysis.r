f8ab68b3299d1b5b61b0fa0bed31ae70f2e203f8
Joel Graycar <jgraycar@berkeley.edu>
2014-08-11 10:52:48 -0700

    working on setup of analysis

--- analysis.r
+++ analysis.r
@@ -1,24 +1,58 @@
-data = read.table("binary-ling-data.data", header=TRUE)
+require("RColorBrewer")
 
-# Analyze the relationship between responses and location.
+binary.data = read.table("binary-ling-data.data", header=TRUE)
+original.data = read.table("ling-data-clean.data", header=TRUE)
+
+# Analyze the relationship between responses and location using binary data.
 # <responses>: a dataframe of the responses to a certain question.
 #              Just a subset of the full binary dataframe.
-# <locations>: the longitude and latitude for all responders.
-analyze <- function(responses, locations) {
-
+# <locations>: a numeric vector containing the longitude and latitude
+#              of all responders.
+analyzeBinary <- function(responses, locations) {
+    
 }
 
-dirty.data = read.table("ling-data-clean.data", header=TRUE)
-cut.data = dirty.data[, seq(5, length(dirty.data) - 2)]
-n.responses = apply(cut.data, 2, max)
+# Analyze the relationship between responses and location using original data.
+# <responses>: a numeric vector giving the response of the responder.
+# <locations>: a numeric vector containing the longitude and latitude
+#              of all responders.
+analyzeOriginal <- function(responses, locations) {
+    n.choices = max(responses)
+    cols = getColors(n.choices)
+}
 
-response.data = data[, seq(5, length(data) - 2)]
-location.data = data[, seq(length(data) - 1, length(data))]
+# Returns a vector of colors given a desired length.
+# Only works for n.choices between 3 - 21, but this works fine for our purposes.
+getColors <- function(n.choices) {
+    if (n.choices > 9) {
+        if (n.choices - 9 < 3) {
+            cols = brewer.pal(n.choices, "Set3")
+        } else {
+            cols = c(brewer.pal(9, "Set1"), brewer.pal(n.choices - 9, "Set3"))
+        }
+    } else {
+        cols = brewer.pal(n.choices, "Set1")
+    }
+    return(cols)
+}
+    
+
+original.responses = original.data[, seq(5, length(original.data) - 2)]
+binary.responses = binary.data[, seq(5, length(binary.data) - 2)]
+
+n.responses = apply(original.responses, 2, max)
+location.data = binary.data[, seq(length(binary.data) - 1, length(binary.data))]
 
 response.index = 1
 
+# Analyze binary data
 for (i in 1..length(n.responses)) {
-    responses = response.data[, seq(response.index, response.index + n.responses[i] - 1)]
-    analyze(responses, location.data)
+    responses = binary.responses[, seq(response.index, response.index + n.responses[i] - 1)]
+    analyzeBinary(responses, location.data)
     response.index = response.index + n.responses[i]
 }
+
+# Analyze original data
+apply(original.responses, 2, function(question.responses) {
+    analyzeOriginal(question.responses, location.data)
+})
